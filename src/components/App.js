f448a6b526f37b19cb715592e858141419918622
DariuszLegizynski <d.legizynski@gmail.com>
2020-05-19 22:25:23 +0200

    Added some new props and also components. Learned about props.children

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
 import React from "react";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
     return(
     <div className="ui container comments">
-        <CommentDetail author="Master" timeAgo="Today at 4:45 PM" comment="Yeah!" avatar={faker.image.avatar()} />
-        <CommentDetail author="Commander" timeAgo="Today at 3:27 AM" comment="Superbbbb" avatar={faker.image.avatar()} />
-        <CommentDetail author="Slave" timeAgo="Yesterday at 1:57 AM" comment="Nice" avatar={faker.image.avatar()} />
+        <ApprovalCard>
+            <div>
+                <h4>Warning!</h4>
+                Are you sure, you want to do this?
+            </div>
+        </ApprovalCard>
+        <ApprovalCard>
+            <CommentDetail author="Master" timeAgo="Today at 4:45 PM" comment="Yeah!" avatar={faker.image.avatar()} />
+        </ApprovalCard>
+        <ApprovalCard>
+            <CommentDetail author="Commander" timeAgo="Today at 3:27 AM" comment="Superbbbb" avatar={faker.image.avatar()} />
+        </ApprovalCard>
+        <ApprovalCard>
+            <CommentDetail author="Slave" timeAgo="Yesterday at 1:57 AM" comment="Nice" avatar={faker.image.avatar()} />
+        </ApprovalCard>
     </div>
     );
 }

fbbfb2b2a6a763caf265de345e1cc5ba0a3cabe1
DariuszLegizynski <d.legizynski@gmail.com>
2020-05-19 15:09:20 +0200

    Refreshed my memory on props

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
-function App(){
+const App = () => {
     return(
-    <div>
-        <h1>Hello Master</h1>
+    <div className="ui container comments">
+        <CommentDetail author="Master" timeAgo="Today at 4:45 PM" comment="Yeah!" avatar={faker.image.avatar()} />
+        <CommentDetail author="Commander" timeAgo="Today at 3:27 AM" comment="Superbbbb" avatar={faker.image.avatar()} />
+        <CommentDetail author="Slave" timeAgo="Yesterday at 1:57 AM" comment="Nice" avatar={faker.image.avatar()} />
     </div>
-    )
+    );
 }
 
 export default App;
